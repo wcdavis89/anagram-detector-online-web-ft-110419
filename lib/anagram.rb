1212ce68969f94ade0d3409bc65254b6d0313ebb
Whittney Davis <wcdavis89@yahoo.com>
2019-11-25 21:57:09 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,8 +1,17 @@
 # Your code goes here!
 class Anagram
   attr_accessor :word 
-  def initialize(word)
-    @word=word
+  def initialize(words)
+    @words=words
   end
   
-  
+  def match(words)
+    words.each do |word| 
+      word.split.each do |letter|
+        if letter.match(words)
+          puts word
+        end
+      end
+    end
+  end
+        
